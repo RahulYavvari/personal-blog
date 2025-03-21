db7f030bac117eacb806cd0a602b1078369242ff
RahulYavvari <rahulyavvari04@gmail.com>
2025-03-21 08:59:28 +0530

    optimizeFonts false

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -9,11 +9,11 @@ const Hero = () => {
         <div className="absolute top-0 left-0 w-full h-full backdrop-blur-[150px] md:backdrop-blur-[250px] -z-10"></div>
         <Image className="absolute h-[80%] md:h-[100vh] md:w-[100vw] top-[50%] left-[50%] tranform translate-x-[-50%] translate-y-[-50%] -z-20" src={wavySphere} priority alt="sphere animation for asthetics"/>
         
-        <div className="dela-gothic--font text-4xl md:text-6xl self-start">
+        <div className="dela-gothic--font text-4xl md:text-5xl self-start">
             <h1 className="mb-2">Hello,</h1>
-            <h1>I'm <span className="text-[#B08EFF]">Rahul Yavvari</span></h1>
+            <h1>I'm <span className="text-[#B08EFF] " >Rahul Yavvari</span></h1>
 
-            <h1 className="mt-10 courier-prime--font text-3xl md:text-5xl text-[#6A6A6A]"><span className="text-[#007E00]"><span className="dela-gothic--font text-2xl md:text-4xl">$/</span></span> 
+            <h1 className="mt-10 courier-prime--font text-3xl md:text-4xl text-[#ffffff]">
                 <Typewriter texts={[" I am a developer.", " I love to code.", " I love design."]} speed={100} loop={true} />
             </h1>
         </div>
