84ecc74ab81e84f3a0551764f2932f07b0ae8bb3
RahulYavvari <rahulyavvari04@gmail.com>
2025-03-21 09:42:39 +0530

    Add theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -15,6 +15,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Rahul Yavvari",
   description: "My blog",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#040c12"
 };
 
 export default function RootLayout({
